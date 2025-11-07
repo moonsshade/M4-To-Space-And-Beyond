f3fcbde0d54ccde2ef7e81738da484e47559bae2
VeeTheWitchy <125075349+moonsshade@users.noreply.github.com>
2025-11-07 12:15:11 -0800

    mid lesson 4

---
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var boost_speed := 1500.0
+var max_speed:= 600.0
+var velocity := Vector2(0, 0)
+
+func _process(delta: float) -> void:	
+	if Input.is_action_just_pressed("boost"):
+		max_speed = boost_speed
+		
+	var direction := Vector2 (0, 0)
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+
+	velocity = direction * max_speed
+	position += velocity * delta
+	if direction.length() > 0.0:
+		rotation = velocity.angle()
